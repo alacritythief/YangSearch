f774906b2f223316dbf649077851d67ed59d5efe
Andy Wong <alacritythief@gmail.com>
2019-05-02 12:55:36 -0400

    updated tests

---
+++ src/components/SearchInput/SearchInput.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchInput from './SearchInput';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<SearchInput />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
