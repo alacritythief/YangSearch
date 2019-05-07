42979e123a2d962a98a8b5e76713cf80763662f6
Andy Wong <alacritythief@gmail.com>
2019-05-06 22:20:42 -0400

    added junk placeholder data

---
+++ src/lib/video.js
@@ -0,0 +1,24 @@
+export const VideoData = [
+  {
+    name: 'Andrew Yang Talks Universal Basic Income, Benefitting From Tech, His Run For President + More',
+    youtubeId: '87M2HwkZZcw',
+    timestamps: [
+      {
+        time: '138',
+        topic: 'ubi',
+        policy: 'ubi',
+        keywords: [
+          'ubi'
+        ]
+      },
+      {
+        time: '256',
+        topic: 'bob',
+        policy: 'bob',
+        keywords: [
+          'thing'
+        ]
+      }
+    ]
+  }
+]
