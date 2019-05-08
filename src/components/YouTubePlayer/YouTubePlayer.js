bc5ba3d2addc70dda8d9398be0ad72b634be4fd6
Andy Wong <alacritythief@gmail.com>
2019-05-08 14:47:56 -0400

    seek time example working

--- src/components/YouTubePlayer/YouTubePlayer.js
+++ src/components/YouTubePlayer/YouTubePlayer.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import YouTube from 'react-youtube';
+import { directive } from '@babel/types';
 
 class YouTubePlayer extends React.Component {
-  // constructor (props) {
-  //   super(props);
-  // }
+  constructor (props) {
+    super(props);
 
-  _onReady (event) {
+    this.state = {
+      videoId: '87M2HwkZZcw', // this.props.videoId
+      seconds: '101', // this.props.second
+      player: null
+    }
+  }
+
+  onReady = (event) => {
     // access to player in all event handlers via event.target
+    this.setState({
+      player: event.target
+    })
     event.target.playVideo();
+  }
+
+  changeTime = () => {
+    if (this.state.player) {
+      console.log('seeking!')
+      this.state.player.seekTo(300)
+      this.state.player.playVideo();
+    }
   }
 
   render () {
@@ -16,16 +34,20 @@ class YouTubePlayer extends React.Component {
       width: '560',
       height: '315',
       playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
+        autoplay: 1,
+        start: this.state.seconds
       }
     };
 
     return (
+      <div>
       <YouTube
-        videoId="87M2HwkZZcw"
+        videoId={this.state.videoId}
         opts={opts}
-        onReady={this._onReady}
+        onReady={this.onReady}
       />
+      <button onClick={this.changeTime}>Change Time</button>
+      </div>
     );
   }
 }
--- src/lib/video.js
+++ src/lib/video.js
@@ -1,24 +1,75 @@
 export const VideoData = [
   {
-    name: 'Andrew Yang Talks Universal Basic Income, Benefitting From Tech, His Run For President + More',
+    name: 'The Breakfast Club: Andrew Yang Interview',
+    description: 'Andrew Yang Talks Universal Basic Income, Benefitting From Tech, His Run For President + More',
     youtubeId: '87M2HwkZZcw',
     timestamps: [
       {
-        time: '138',
-        topic: 'ubi',
-        policy: 'ubi',
+        time: '0:00:23',
+        seconds: '23',
+        topic: 'Who is Andrew Yang?',
+        policy: null,
         keywords: [
-          'ubi'
+          'entrepreneur',
+          'Venture for America',
+          'VFA',
+          'Donald Trump',
+          'Trump',
+          'manufacturing',
+          'jobs',
+          'automation'
         ]
       },
       {
-        time: '256',
-        topic: 'bob',
-        policy: 'bob',
+        time: '0:01:41',
+        seconds: '84',
+        topic: 'Won\'t new jobs be created from automation that will offset its displacement?',
+        policy: null,
         keywords: [
-          'thing'
+          'automation',
+          'retraining',
+          'unemployment',
+          'disability',
+          'overdose',
+          'suicide',
+          'displacement',
+          'immigrants',
+          'labor'
+        ]
+      },
+      {
+        time: '0:03:23',
+        seconds: '193',
+        topic: 'What is the solution to automation displacing workers?',
+        policy: 'The Freedom Dividend',
+        keywords: [
+          'Freedom Dividend',
+          'Universal Basic Income',
+          'UBI',
+          'Alaska',
+          'petroleum dividend',
+          'oil dividend',
+          'income inequality',
+          'technology money'
         ]
       }
     ]
   }
 ]
+
+// TEMPLATE
+// {
+//   name: 'Shorthand name of video',
+//   description: 'Acutal title of youtube video',
+//   youtubeId: '87M2HwkZZcw',
+//   timestamps: [
+//     {
+//       time: '235',
+//       topic: 'Self Driving Vehicles',
+//       policy: 'The Freedom Dividend',
+//       keywords: [
+//         'cars'
+//       ]
+//     }
+//   ]
+// }
