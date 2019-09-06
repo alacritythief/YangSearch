622f585db135c948b4b764b080bfe82e131781ab
Andy Wong <alacritythief@gmail.com>
2019-09-06 17:33:42 -0400

    added coverage script and enzyme for future tests

--- src/components/PolicyResults/PolicyResults.test.js
+++ src/components/PolicyResults/PolicyResults.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import PolicyResults from './PolicyResults';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
+  shallow(
     <PolicyResults 
       searchValue={''}
       policies={[]} 
-    />, div
+    />
   );
-  ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/SearchInput/SearchInput.test.js
+++ src/components/SearchInput/SearchInput.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import SearchInput from './SearchInput';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchInput/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<SearchInput/>);
 });
